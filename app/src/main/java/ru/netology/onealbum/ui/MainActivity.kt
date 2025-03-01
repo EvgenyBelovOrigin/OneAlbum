9a3204d1cfa5c7f2b2d85632dbfbf45c6c84d53c
Evgeny <e.belov@inbox.ru>
2025-03-01 22:35:03 +0300

    card song done at all

--- app/src/main/java/ru/netology/onealbum/ui/MainActivity.kt
+++ app/src/main/java/ru/netology/onealbum/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.netology.onealbum.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.onealbum.R
 import ru.netology.onealbum.databinding.ActivityMainBinding
 import ru.netology.onealbum.viewmodel.ViewModel
 
@@ -14,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
 
         val viewModel: ViewModel by viewModels()
         viewModel._album.observe(this) { album ->
